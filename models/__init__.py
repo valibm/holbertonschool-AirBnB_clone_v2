5768358c2986be150d562a3968f36538deb442f2
valibm <vmustafayev2001@gmail.com>
2024-04-21 18:37:19 +0000

    Added necessary files

---
+++ models/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This module allows to change the storage type"""
+from os import getenv
+
+
+if getenv("HBNB_TYPE_STORAGE") == "db":
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+storage.reload()
